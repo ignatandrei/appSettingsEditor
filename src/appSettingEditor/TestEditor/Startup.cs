14b3d5eb072e6308a2f2e1520c74151a0babe629
ignatandrei <ignatandrei@yahoo.com>
2021-03-21 21:26:12 +0200

    put in the namespace

--- src/appSettingEditor/TestEditor/Startup.cs
+++ src/appSettingEditor/TestEditor/Startup.cs
@@ -11,7 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using TestEditor.SettingsJson;
 namespace TestEditor
 {
     public class Startup
