f40e4b33934b001c9053fa396698c32adfcb07c0
ignatandrei <ignatandrei@yahoo.com>
2021-03-22 22:23:44 +0200

    added diagnostics

--- src/appSettingEditor/appSettingEditorAPI/AppSettingsGenerator.cs
+++ src/appSettingEditor/appSettingEditorAPI/AppSettingsGenerator.cs
@@ -12,62 +12,80 @@ namespace appSettingEditorAPI
     [Generator]
     public class AppSettingsGenerator : ISourceGenerator
     {
+
         public void Initialize(GeneratorInitializationContext context)
         {
             //Debugger.Launch();
         }
+        Diagnostic DoDiagnostic(DiagnosticSeverity ds, string message)
+        {
+            //info  could be seen only with 
+            // dotnet build -v diag
+            var dd = new DiagnosticDescriptor(ThisAssembly.Info.Product, $"StartExecution", $"{message}", "SkinnyControllers", ds, true);
+            var d = Diagnostic.Create(dd, Location.None, "andrei.txt");
+            return d;
+        }
         public void Execute(GeneratorExecutionContext context)
         {
+            try
+            {
+                string namespaceName = context.Compilation?.AssemblyName;
 
-            string namespaceName = context.Compilation?.AssemblyName;
+                //var settings = context.AdditionalFiles.FirstOrDefault(file =>
+                //        file.Path.EndsWith(".json", StringComparison.InvariantCultureIgnoreCase)
+                //        &&
+                //        file.Path.IndexOf("appsettings", StringComparison.InvariantCultureIgnoreCase) > -1
+                //        );
 
-            //var settings = context.AdditionalFiles.FirstOrDefault(file =>
-            //        file.Path.EndsWith(".json", StringComparison.InvariantCultureIgnoreCase)
-            //        &&
-            //        file.Path.IndexOf("appsettings", StringComparison.InvariantCultureIgnoreCase) > -1
-            //        );
+                var allSettings = context.AdditionalFiles.Where(
+                    file =>
+                    {
+                        var f = context.AnalyzerConfigOptions.GetOptions(file);
+                        if (!f.TryGetValue("build_metadata.AdditionalFiles.generateAPI", out string val))
+                            return false;
 
-            var allSettings = context.AdditionalFiles.Where(
-                file =>
-                {
-                    var f = context.AnalyzerConfigOptions.GetOptions(file);
-                    if (!f.TryGetValue("build_metadata.AdditionalFiles.generateAPI", out string val))
-                        return false;
-
-                    return (val == "true");
-                }
-                ).ToArray();
+                        return (val == "true");
+                    }
+                    ).ToArray();
                 ;
 
-            if (!allSettings.Any())
-            {
-                //do diagnostics
-                return;
-            }
-            foreach (var settings in allSettings)
-            {
-
-
-                string nameSettings = Path.GetFileNameWithoutExtension(settings.Path);
-                //TODO : diagnostic
-                var jsonFileText = settings.GetText();
-                if (jsonFileText == null)
+                if (!allSettings.Any())
                 {
-                    //TODO: diagnostic
+                    context.ReportDiagnostic(DoDiagnostic(DiagnosticSeverity.Warning,
+                        $"do not have an appsettings.json with generateAPI"));
                     return;
                 }
-                //var generatedCode = GenerateFile(jsonFileText.ToString(), namespaceName);
-                //var g = new GeneratorData();
-                //var generatedCode= g.GenerateFile(jsonFileText.ToString(),namespaceName);
-                //context.AddSource($"{nameSettings}.gen.cs", SourceText.From(generatedCode, Encoding.UTF8));
-                //var json = JsonDocument.Parse(jsonFileText.ToString());
+                foreach (var settings in allSettings)
+                {
 
-                var g = new GeneratorFromJSON(); 
-                var generatedCode = g.GenerateFile(jsonFileText.ToString(), nameSettings, namespaceName, "appsettings.txt");
-                context.AddSource($"{nameSettings}.gen.cs", SourceText.From(generatedCode, Encoding.UTF8));
+                    string nameSettings = Path.GetFileNameWithoutExtension(settings.Path);
+                    //TODO : diagnostic
+                    var jsonFileText = settings.GetText();
+                    if (jsonFileText == null)
+                    {
+                        context.ReportDiagnostic(DoDiagnostic(DiagnosticSeverity.Warning,
+        $"is this any content in {nameSettings} ?"));
 
-                generatedCode = g.GenerateFile(jsonFileText.ToString(), nameSettings, namespaceName, "Controller.txt");
-                context.AddSource($"{nameSettings}Controller.gen.cs", SourceText.From(generatedCode, Encoding.UTF8));
+                        return;
+                    }
+                    //var generatedCode = GenerateFile(jsonFileText.ToString(), namespaceName);
+                    //var g = new GeneratorData();
+                    //var generatedCode= g.GenerateFile(jsonFileText.ToString(),namespaceName);
+                    //context.AddSource($"{nameSettings}.gen.cs", SourceText.From(generatedCode, Encoding.UTF8));
+                    //var json = JsonDocument.Parse(jsonFileText.ToString());
+
+                    var g = new GeneratorFromJSON();
+                    var generatedCode = g.GenerateFile(jsonFileText.ToString(), nameSettings, namespaceName, "appsettings.txt");
+                    context.AddSource($"{nameSettings}.gen.cs", SourceText.From(generatedCode, Encoding.UTF8));
+
+                    generatedCode = g.GenerateFile(jsonFileText.ToString(), nameSettings, namespaceName, "Controller.txt");
+                    context.AddSource($"{nameSettings}Controller.gen.cs", SourceText.From(generatedCode, Encoding.UTF8));
+                }
+            }
+            catch(Exception ex)
+            {
+                context.ReportDiagnostic(DoDiagnostic(DiagnosticSeverity.Error,$"error:"+ex.Message));
+
             }
         }
 
